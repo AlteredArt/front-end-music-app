7cd6fe927a36d51b0165df3641f3fd2da1247bcc
Altered Art <cherokeedreemer@gmail.com>
2020-02-11 10:27:38 -0700

    doiing work

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 console.log("music")
 
-// fetch('')
-//     .then(response => response.json())
-//     .then(heroines => {
+fetch('http://localhost:3000/users')
+    .then(response => response.json())
+    .then(users => users.map(showuser)
+
+
+function showuser(user)
+const userPlaylist = user.userPlaylist
+let h1 = document.createElement('h1')
+h1.innerHTML = `<a href=''`
