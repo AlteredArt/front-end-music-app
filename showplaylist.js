bc0a3863e38c5e6ec4de75f963a92f037f99bb5a
Altered Art <cherokeedreemer@gmail.com>
2020-02-11 14:05:01 -0700

    hey jorge

--- showplaylist.js
+++ showplaylist.js
@@ -1,1 +1,20 @@
 console.log("show playlist page")
+
+let params = new URLSearchParams(document.location.search)
+const id = params.get('id')
+
+fetch(`http://localhost:3000/playlists/${id}`)
+    .then(response => response.json())
+    .then(showplaylist)
+
+
+function showplaylist(playlist){
+    let h1 = document.createElement('h1')
+    h1.innerText = `${playlist.name}`
+    // console.log(playlist.name)
+    document.body.appendChild(h1)
+    playlist.song.
+    let p = document.createElement('p')
+    p.innerHTML = `<a href='showsong.html?=id${playlist.song.title}'> ${playlist.song.title}</a>`
+    document.appendChild(p)
+}
--- showuser.js
+++ showuser.js
@@ -1,17 +1,20 @@
 console.log('show user page')
-let params = new URLSearchParams(document.location.seacrh)
+let params = new URLSearchParams(document.location.search)
 const id = params.get('id')
 
 fetch(`http://localhost:3000/users/${id}`)
     .then(response => response.json())
-    .then(Users => Users.map(showuser))
-    
+    .then(showuser)
+
     function showuser(user){
-        const userPlay = user.playlist
+        // const userPlay = user.playlists
         let h1 = document.createElement('h1')
         h1.innerText = `${user.name}`
-        let p = document.createElement('p')
-        p.innerHTML = `<a href='showplaylist.html?id=${playlist.id}'> ${userPlay.name}</a>`
-        document.body.append(h1,p)
+        document.body.appendChild(h1)
+        user.playlists.map(playlist =>{
+            let p = document.createElement('p')
+            p.innerHTML = `<a href='showplaylist.html?id=${playlist.id}'> ${playlist.name}</a>`
+            document.body.appendChild(p)
+        })
 
     }
