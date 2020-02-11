cf9792866804b3c1d34426281598393e4b7bdc72
Altered Art <cherokeedreemer@gmail.com>
2020-02-11 13:16:34 -0700

    little steps

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-console.log("music")
+console.log("sign in page")
 
-fetch('http://localhost:3000/users')
-    .then(response => response.json())
-    .then(users => users.map(showuser)
+// fetch('http://localhost:3000/users')
+//     .then(response => response.json())
+//     .then(users => users.map(showuser)
 
 
-function showuser(user)
-const userPlaylist = user.userPlaylist
-let h1 = document.createElement('h1')
-h1.innerHTML = `<a href=''`
+// function showuser(user)
+// const userPlaylist = user.userPlaylist
+// let h1 = document.createElement('h1')
+// h1.innerHTML = `<a href='showuser.html?id=${user.id}'> ${user.name}, ${user.username}</a>`
+
+// fetch("http://localhost:3000/users")
+//     .then(response => response.json())
+//     .then(users =>{
+//         users.map(user =>{
+//             let h1 = document.createElement('h1')
+//             h1.innerHTML = `<a href='showuser.html?id=${user.id}'></a>`
+//             document.body.appendChild(h1)
+//         })   
+//     })
+
--- showsong.js
+++ showsong.js
@@ -1,1 +1,12 @@
 console.log('show song page')
+
+let params = new URLSearchParams(document.location.seacrh)
+const id = params.get('id')
+
+fetch(`http://localhost:3000/songs/${id}`)
+    .then(response => response.json())
+    .then(songs => songs.map(showsong)
+
+function showsong(song)
+    let p = document.createElement('p')
+    h1.innerHTML = `<a href='showsong.html?id=${song.id}'> ${song.title}, ${song.artist}, ${song.album}, $song.duration}</a>`
