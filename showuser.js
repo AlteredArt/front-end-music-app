79fa7b8d8a8c71222469d2ed16115eec4f53289a
Altered Art <cherokeedreemer@gmail.com>
2020-02-12 17:42:10 -0700

    styling feb 12

--- showuser.js
+++ showuser.js
@@ -1,29 +1,63 @@
 console.log('show user page')
 let params = new URLSearchParams(document.location.search)
 const id = params.get('id')
-
 fetch(`http://localhost:3000/users/${id}`)
     .then(response => response.json())
     .then(showuser)
-
     function showuser(user){
+        const showuserul = document.querySelector("#showuser-ul")
         let h1 = document.createElement('h1')
         h1.innerText = `${user.username}`
-       
-        user.playlists.map(playlist =>{
+          user.playlists.map(playlist =>{
             let p = document.createElement('p')
             p.innerHTML = `<a href='showplaylist.html?id=${playlist.id}'> ${playlist.name}</a>`
-           
-        document.body.append(h1, p)
-        })
+            showuserul.append(h1, p)
+        })    }
+ 
 
-    }
-    function myHome() {
-        location.replace(`http://localhost:3001/showuser.html?id=${id}`)
-      }
-      function myPlaylist() {
-        location.replace(`http://localhost:3001/showplaylist.html?id=${id}`)
-      }
-      function mySong() {
-        location.replace(`http://localhost:3001/showsong.html?id=${id}`)
-      }
+
+
+
+    // const ul = document.querySelector('ul')
+    // fetch("http://localhost:3000/playlists")
+    //   .then(response => response.json())
+    //   // .then(console.log)
+    //   .then(playlist => playlist 
+    //     .map(userToElement)
+    //     .map(userUpdate)
+    //     .map(userDelete)
+    //     .forEach(appendToUl)
+    // )
+    // function userToElement(playlist) {
+    //     const li = document.createElement('li')
+    //     li.innerHTML = playlist.name
+    //     li.dataset.id = playlist.id
+    //     li.dataset.name = playlist.name
+    //     return li
+    // }
+    // function appendToUl(li) {
+    //     ul.append(li)
+    // }
+    // function userDelete(li) {
+    //     const form = document.createElement("form")
+    //     form.action = `http://localhost:3000/playlists/${li.dataset.id}`
+    //     form.method = "POST"
+    //     form.innerHTML = `
+    //     <input type="submit" value="Delete this Playlist"/>
+    //     <input type="hidden" name="_method" value="delete"/>
+    //     `
+    //     li.append(form)
+    //     return li
+    // }
+    // function userUpdate(li) {
+    //     const form = document.createElement("form")
+    //     form.action = `http://localhost:3000/playlists/${li.dataset.id}`
+    //     form.method = "POST"
+    //     form.innerHTML = `
+    //             <input type="text" name="name" placeholder="Name" value=${li.dataset.name}/>
+    //             <input type="submit" value="Update this Playlist"/>
+    //             <input type="hidden" name="_method" value="put"/>
+    //         `
+    //     li.append(form)
+    //     return li
+    // }
