5f1fc3432d5da6542c1b63289ed63760aae1dccb
Altered Art <cherokeedreemer@gmail.com>
2020-02-13 16:58:48 -0700

    jares css push

--- showsong.js
+++ showsong.js
@@ -10,15 +10,15 @@ fetch(`http://localhost:3000/songs/${id}`)
 console.log('show song page')
 function showsinglesong(song){
     const SongDiv = document.querySelector("#song-div")
-    let h1 = document.createElement('h1')
+    let title = document.createElement('h1')
     let artist = document.createElement('p')
     let album = document.createElement('p')
     let duration = document.createElement('p')
 
-    h1.innerText = song.title
+    title.innerText = song.title
     artist.innerText = song.artist
     album.innerText = song.album
     duration.innerText = song.duration
 
-    SongDiv.append(h1, artist, album, duration)
+    SongDiv.append(title, artist, album, duration)
 }
